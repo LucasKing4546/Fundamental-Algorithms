ebad1d953fcb3495bf6607d08ce6e7a471cbbd67
Secară Lucas <lucassecara@yahoo.com>
2024-12-18 19:36:42 +0200

    Added backtracking function and a function which return the set of diseases from a department

--- Applications/Hospital Departments and Patients/utilities/utility.py
+++ Applications/Hospital Departments and Patients/utilities/utility.py
@@ -9,3 +9,55 @@ def sorting (arr : list[Patient | Departments], key = lambda x: x, relation = la
 
 def filtering (arr : list[Patient | Departments], key = lambda x: x):
     return [x for x in arr if key(x)]
+
+def backtrack(k: int, current_group: list[Patient|Departments], start: int, set_identifier: set, elements: list[Patient|Departments], results, key = lambda x: x):
+    if len(current_group) == k:
+        results.append(current_group[:])
+        return
+    for i in range(start, len(elements)):
+        element = elements[i]
+        if key(element) not in set_identifier:
+            current_group.append(element)
+            set_identifier.add(key(element))
+            backtrack(k, current_group, i + 1, set_identifier, elements, results, key)
+            current_group.pop()
+            set_identifier.remove(key(element))
+
+def different_diseases(department: Departments):
+    diseases = set()
+    for patient in department.get_list_of_patients():
+        diseases.add(patient.get_disease())
+    return diseases
+
+
+'''
+
+def backtrack_patients(k: int, current_group: list[Patient], start: int, set_pnc: set, patients: list[Patient], results):
+    if len(current_group) == k:
+        results.append(current_group)
+        return
+    for i in range(start, len(current_group)):
+        patient = patients[i]
+        if patient.get_pnc() not in set_pnc:
+            current_group.append(patient)
+            set_pnc.add(patient.get_pnc())
+            backtrack_patients(k, current_group, i + 1, set_pnc, patients, results)
+            current_group.pop()
+            set_pnc.remove(patient.get_pnc())
+    return results
+
+def backtrack_departments(k: int, current_group: list[Departments], start: int, set_id: set, departments: list[Departments], results):
+    if len(current_group) == k:
+        results.append(current_group)
+        return
+    for i in range(start, len(current_group)):
+        department = departments[i]
+        if department.get_id() not in set_id:
+            current_group.append(department)
+            set_id.add(department.get_id())
+            backtrack_departments(k, current_group, i + 1, set_id, departments, results)
+            current_group.pop()
+            set_id.remove(department.get_id())
+    return results
+
+'''
