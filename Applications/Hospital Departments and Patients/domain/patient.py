c098349042191f293f8a563035d187de385e8f8f
Secară Lucas <lucassecara@yahoo.com>
2024-12-21 17:21:57 +0200

    Added __repr__ and __eq__ functions

--- Applications/Hospital Departments and Patients/domain/patient.py
+++ Applications/Hospital Departments and Patients/domain/patient.py
@@ -48,3 +48,16 @@ class Patient:
         if not isinstance(age, int):
             raise ValueError('Age must be a number')
         self.__age = age
+
+
+    def __repr__(self):
+        return f"Patient: {self.__first_name} {self.__last_name}, PNC: {self.__pnc}, Disease: {self.__disease}, Age: {self.__age}\n"
+
+    def __eq__(self, other):
+        if not isinstance(other, Patient):
+            return False
+        return (self.__first_name == other.__first_name and
+                self.__last_name == other.__last_name and
+                self.__pnc == other.__pnc and
+                self.__disease == other.__disease and
+                self.__age == other.__age)
